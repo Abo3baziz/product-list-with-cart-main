eaf595824d12c34f031ba400b86b795fb7575f02
Ahmed Aziz <azez55.ali@gmail.com>
2025-08-17 16:21:29 +0300

    refactor: app.jsx to include item and itemWrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import Heading from './components/Heading';
+import data from './assets/data.json';
+import Item from './components/Item';
+import ItemsWrapper from './components/ItemsWrapper';
+
 function App() {
-  return <></>;
+  return (
+    <main className="h-screen p-15 bg-[var(--Rose-50)] overflow-y-auto">
+      <Heading cssClasses={'text-4xl font-bold mb-5'} />
+      <ItemsWrapper>
+        {data.map((item, index) => {
+          return (
+            <Item
+              name={item.name}
+              image={item.image}
+              category={item.category}
+              price={item.price}
+              key={index}
+            />
+          );
+        })}
+      </ItemsWrapper>
+    </main>
+  );
 }
 
 export default App;
