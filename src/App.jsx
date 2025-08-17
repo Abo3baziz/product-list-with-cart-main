b669d3ffe19876e643fff163bccb58c9dcd6f98f
Ahmed Aziz <azez55.ali@gmail.com>
2025-08-17 22:22:33 +0300

    refactor: App.jsx to include Cart Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,34 @@ import Heading from './components/Heading';
 import data from './assets/data.json';
 import Item from './components/Item';
 import ItemsWrapper from './components/ItemsWrapper';
+import Cart from './components/Cart';
+import { CartContextProvider } from './context/cartContext';
 
 function App() {
   return (
-    <main className="h-screen p-15 bg-[var(--Rose-50)] overflow-y-auto">
-      <Heading cssClasses={'text-4xl font-bold mb-5'} />
-      <ItemsWrapper>
-        {data.map((item, index) => {
-          return (
-            <Item
-              name={item.name}
-              image={item.image}
-              category={item.category}
-              price={item.price}
-              key={index}
-            />
-          );
-        })}
-      </ItemsWrapper>
-    </main>
+    <CartContextProvider>
+      <main className="h-screen p-15 bg-[var(--Rose-50)] overflow-y-auto grid grid-cols-3">
+        <div className="flex flex-col col-span-2">
+          <Heading cssClasses={'text-4xl font-bold mb-5 w-fit'}>
+            Desserts
+          </Heading>
+          <ItemsWrapper>
+            {data.map((item, index) => {
+              return (
+                <Item
+                  name={item.name}
+                  image={item.image}
+                  category={item.category}
+                  price={item.price}
+                  key={index}
+                />
+              );
+            })}
+          </ItemsWrapper>
+        </div>
+        <Cart />
+      </main>
+    </CartContextProvider>
   );
 }
 
