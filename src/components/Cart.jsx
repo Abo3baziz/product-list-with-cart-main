0bf76432d42b3c78b79b43a52fc557281667f748
Ahmed Aziz <azez55.ali@gmail.com>
2025-08-20 14:37:33 +0300

    refactor: cart component to display cart item's quantity

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,15 @@
 import Heading from './Heading';
 import emptyCartImage from '../assets/images/illustration-empty-cart.svg';
+import { useContext } from 'react';
+import CartContext from '../context/cartContext';
 
 function Cart() {
+  const { items } = useContext(CartContext);
+
+  let count = items.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.quantity;
+  }, 0);
+
   let noItemsSelected = (
     <div className="flex items-center justify-center flex-col">
       <img src={emptyCartImage} alt="empty cart image" className="mb-5" />
@@ -12,9 +20,9 @@ function Cart() {
   return (
     <div className="bg-white rounded-2xl p-7 h-fit">
       <Heading cssClasses={'font-bold text-2xl text-(--Red) mb-10'}>
-        Your Cart (0)
+        Your Cart ({count})
       </Heading>
-      {noItemsSelected}
+      {count >= 1 ? <ul></ul> : noItemsSelected}
     </div>
   );
 }
