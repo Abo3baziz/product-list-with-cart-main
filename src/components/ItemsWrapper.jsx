9f5621191321bcfe50cfe0f5834c4498118881b5
Ahmed Aziz <azez55.ali@gmail.com>
2025-08-21 19:47:08 +0300

    fix: component style

--- src/components/ItemsWrapper.jsx
+++ src/components/ItemsWrapper.jsx
@@ -1,5 +1,9 @@
 function ItemsWrapper({ children }) {
-  return <div className="grid grid-cols-3 gap-7 mr-8">{children}</div>;
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 mr-0 lg:mr-8">
+      {children}
+    </div>
+  );
 }
 
 export default ItemsWrapper;
