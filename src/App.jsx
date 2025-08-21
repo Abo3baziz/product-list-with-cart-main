89b3f90061e6521917208ed8afe9046176c81a4d
Ahmed Aziz <azez55.ali@gmail.com>
2025-08-21 19:52:02 +0300

    fix: import url & styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import data from './assets/data.json';
 import Item from './components/Item';
 import ItemsWrapper from './components/ItemsWrapper';
 import Cart from './components/Cart';
-import { CartContextProvider } from './context/cartContext';
+import { CartContextProvider } from './context/CartContext';
 
 function App() {
   return (
     <CartContextProvider>
-      <main className="h-screen p-15 bg-[var(--Rose-50)] overflow-y-auto grid grid-cols-3">
-        <div className="flex flex-col col-span-2">
-          <Heading cssClasses={'text-4xl font-bold mb-5 w-fit'}>
+      <main className="min-h-screen p-6 md:p-10 bg-[var(--Rose-50)] overflow-y-auto grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="flex flex-col lg:col-span-2">
+          <Heading cssClasses={'text-3xl md:text-4xl font-bold mb-5 w-fit'}>
             Desserts
           </Heading>
           <ItemsWrapper>
