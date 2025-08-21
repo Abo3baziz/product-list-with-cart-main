0bde07c62c2c5eaeb18b4d25c1dfe53dedecb110
Ahmed Aziz <azez55.ali@gmail.com>
2025-08-21 19:46:23 +0300

    fix: component styles

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -4,7 +4,7 @@ function AddButton({ onAdd }) {
   return (
     <button
       onMouseDown={onAdd}
-      className="mt-2 w-fit rounded-[50px] border bg-white relative bottom-2 left-[50%] -translate-[50%] py-3 px-8 hover:text-(--Red) duration-200 ease-in cursor-pointer"
+      className="mt-2 w-[200px] rounded-[50px] border bg-white relative bottom-2 left-[50%] translate-[-50%] py-3 px-8 hover:text-[var(--Red)] duration-200 ease-in cursor-pointer"
     >
       <img src={cartIcon} alt="cart icon" className="inline mr-1" /> Add to Cart
     </button>
